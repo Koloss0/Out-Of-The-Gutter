626fa02257d264f4bca98477ebe9e73083d5499c
MJSD <math_jon207@hotmail.com>
2024-02-24 16:09:10 -0600

    connected net signals

--- Scripts/GamePage.gd
+++ Scripts/GamePage.gd
@@ -1,10 +1,16 @@
 extends Node
 
 @onready var world: Node2D = $World
+@onready var start_button: TextureButton = $Control/StartButton
+@onready var counter: Control = $Control/Counter
+@onready var platform_generator: Node2D = $World/PlatformGenerator
+
+const MAP_HEIGHT : int = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Net.peer_connected.connect(on_peer_connected)
+	Net.map_seed_received.connect(on_map_seed_received)
 	Net.player_registered.connect(on_player_registered)
 	Net.player_deregistered.connect(on_player_deregistered)
 	Net.peer_disconnected.connect(on_peer_disconnected)
@@ -25,12 +31,24 @@ func on_peer_connected(peer_id: int):
 func on_player_registered(peer_id: int):
 	var player_info = Net.player_data[peer_id]
 	world.spawn_player(player_info)
+	
+	if is_multiplayer_authority() and Net.num_players > 1:
+		start_button.show()
+		start_button.set_disabled(false)
+		
 
 
 func on_player_deregistered(peer_id: int):
 	world.remove_player(peer_id)
 	AlertDisplayer.alert("Player %s Disconnected" % peer_id)
+	
+	if is_multiplayer_authority() and Net.num_players <= 1:
+		start_button.hide()
+		start_button.set_disabled(true)
 
+func on_map_seed_received(seed: int):
+	platform_generator.generate_map(MAP_HEIGHT, seed)
+	get_tree().call_group("Platforms", "set_collision", false)
 
 func on_peer_disconnected(peer_id: int):
 	pass
@@ -43,3 +61,13 @@ func on_connection_failed():
 
 func on_server_disconnected():
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	start_countdown.rpc()
+
+@rpc
+func start_countdown():
+	counter.play_countdown()
+	get_tree().call_group("Platforms", "set_collision", true)
+	
