5d88ef9386a117ca52786e1d70160cc0e7700da7
Logan Furedi <furedil@myumanitoba.ca>
2024-02-24 12:53:45 -0600

    networking

--- Scripts/GamePage.gd
+++ Scripts/GamePage.gd
@@ -4,7 +4,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Net.peer_connected.connect(on_peer_connected)
 	Net.start()
 	MusicPlayer.play_Lobby_music()
 	var my_player_info = PlayerInfo.new(multiplayer.get_unique_id())
 	world.spawn_player(my_player_info)
+
+func on_peer_connected(peer_id: int):
+	var player_info = PlayerInfo.new(peer_id)
+	world.spawn_player(player_info)
