7a8e556fd255ec48489507baffc70a3c99437480
Logan Furedi <furedil@myumanitoba.ca>
2024-02-24 12:21:21 -0600

    Changes to Menus

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
-func spawn_player():
-	
+const player_scene = preload("res://Scenes/player.tscn")
+@onready var spawnpoint: Node2D = $Spawnpoint
+@onready var players: Node2D = $Players
+
+func spawn_player(player_info: PlayerInfo):
+	var player = player_scene.instantiate()
+	players.add_child(player)
+	player.position = spawnpoint.position
+	player.init(player_info)
