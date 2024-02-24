eb4863d25ed0eee3a407f1db655a191a80742293
Logan Furedi <furedil@myumanitoba.ca>
2024-02-24 10:46:50 -0600

    Networking work

--- Autoloads/Net.gd
+++ Autoloads/Net.gd
@@ -9,12 +9,22 @@ var network_peer := ENetMultiplayerPeer.new()
 
 var is_host := false
 
-var server_ip: int
+var server_ip: String
 var server_port: int
+
+signal connected_to_server()
+signal connection_failed()
+signal peer_connected(peer_id: int)
+signal peer_disconnected(peer_id: int)
+signal server_disconnected()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	multiplayer.connected_to_server.connect(on_connected_to_server)
+	multiplayer.connection_failed.connect(on_connection_failed)
+	multiplayer.peer_connected.connect(on_peer_connected)
+	multiplayer.peer_disconnected.connect(on_peer_disconnected)
+	multiplayer.server_disconnected.connect(on_server_disconnected)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,6 +41,30 @@ func start():
 			print("[Net] Hosting Server...")
 		else:
 			print("[Net] Failed to create server: %s" % error);
-	#else:
-		#error = network_peer.create_client()
-		
+	else:
+		error = network_peer.create_client(server_ip, server_port)
+		if error == OK:
+			multiplayer.set_network_peer(network_peer)
+			print("[Net] Joining...")
+		else:
+			print("[Net] Failed to create client: %s" % error);
+
+func on_connected_to_server():
+	connected_to_server.emit()
+	pass
+	
+func on_connection_failed():
+	connection_failed.emit()
+	pass
+
+func on_peer_connected(peer_id: int):
+	peer_connected.emit(peer_id)
+	pass
+
+func on_peer_disconnected(peer_id: int):
+	peer_disconnected.emit(peer_id)
+	pass
+
+func on_server_disconnected():
+	server_disconnected.emit()
+	pass
