88f0d76081371c938f87c198ae92e153cfc047ce
Logan Furedi <furedil@myumanitoba.ca>
2024-02-24 10:23:32 -0600

    Add Net script

--- Autoloads/Net.gd
+++ Autoloads/Net.gd
@@ -1,6 +1,16 @@
 # manages networking stuff
 extends Node
 
+const MAX_PLAYERS = 4
+
+var player_data: Array[PlayerInfo]
+
+var network_peer := ENetMultiplayerPeer.new()
+
+var is_host := false
+
+var server_ip: int
+var server_port: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +20,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func start():
+	var error = OK
+	if is_host:
+		error = network_peer.create_server(server_port, MAX_PLAYERS-1)
+		if error == OK:
+			multiplayer.set_network_peer(network_peer)
+			print("[Net] Hosting Server...")
+		else:
+			print("[Net] Failed to create server: %s" % error);
+	#else:
+		#error = network_peer.create_client()
+		
