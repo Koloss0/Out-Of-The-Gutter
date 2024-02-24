20eb9609335a44141dff9c9d7491d55cb758006b
Pratham <pratham111555@gmail.com>
2024-02-24 10:21:59 -0600

    Moving platform script added

---
+++ Scripts/moving_platform.gd
@@ -0,0 +1,24 @@
+extends AnimatableBody2D
+
+const SPEED = 13.00
+
+var left_bound = 240
+
+@onready var right_bound  = get_viewport_rect().size.x - 240
+# Called when the node enters the scene tree for the first time.
+
+var moving_right = false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if(position.x > right_bound):
+		moving_right = false
+	
+	if(position.x < left_bound):
+		moving_right = true
+	
+	if(moving_right): 
+		translate(Vector2(SPEED, 0))
+	else:
+		translate((Vector2(-SPEED, 0)))
+		
